7dad7f5952b05365f35ddd8aadc3293b4d241c70
Luis Pabón <lpabon@purestorage.com>
2021-11-01 23:17:35 -0400

    save

---
+++ tool/pxt/main.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"context"
+	//"encoding/json"
+	"flag"
+	"time"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+
+	"github.com/gravitational/teleport/api/client"
+	"github.com/sirupsen/logrus"
+
+	teleport "github.com/gravitational/teleport/api/client"
+	tc "github.com/gravitational/teleport/lib/client"
+	"github.com/gravitational/teleport/lib/client/identityfile"
+)
+
+type Opts struct {
+	Cluster               string
+	TeleportProxyPort     string
+	TeleportServer        string
+	TeleportKubeProxyPort string
+	TeleportIdentityFile  string
+}
+
+var (
+	opts Opts
+)
+
+func init() {
+	flag.StringVar(&opts.Cluster, "cluster", "", "K8S Cluster name")
+	flag.StringVar(&opts.TeleportProxyPort, "proxy-port", "3080", "Teleport proxy port")
+	flag.StringVar(&opts.TeleportServer, "server", "ip-70-0-0-129.brbnca.spcsdns.net", "Teleport server")
+	flag.StringVar(&opts.TeleportKubeProxyPort, "kube-proxy-port", "3026", "Teleport proxy server")
+	flag.StringVar(&opts.TeleportIdentityFile, "identity", "", "Teleport identity file")
+}
+
+func main() {
+	flag.Parse()
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute*1)
+	defer cancel()
+
+	// Load teleport credentials from $HOME/.tsh
+	creds := teleport.LoadProfile("", "")
+
+	// Use a teleport cert identity file if supplied
+	if opts.TeleportIdentityFile != "" {
+		creds = teleport.LoadIdentityFile(opts.TeleportIdentityFile)
+	}
+
+	// Connect to Teleport
+	proxy := opts.TeleportServer + ":" + opts.TeleportProxyPort
+	clt, err := teleport.New(ctx, client.Config{
+		Addrs: []string{
+			proxy,
+		},
+		Credentials: []client.Credentials{
+			creds,
+		},
+		InsecureAddressDiscovery: true,
+	})
+	if err != nil {
+		logrus.Fatalf("failed to load teleport client: %v", err)
+	}
+	defer clt.Close()
+
+	// Test Teleport connection
+	resp, err := clt.Ping(ctx)
+	if err != nil {
+		logrus.Fatalf("failed to ping server %s: %v", proxy, err)
+	}
+	logrus.Infof("Teleport server response: %v", resp)
+
+	// Get Kubernetes services
+	services, err := clt.GetKubeServices(ctx)
+	if err != nil {
+		logrus.Fatalf("failed to get kube services: %v", err)
+	}
+
+	// Choose a cluster
+	//kubeCluster := opts.
+	for _, service := range services {
+		clusters := service.GetKubernetesClusters()
+
+		// Show available k8s clusters
+		for _, cluster := range clusters {
+			logrus.Infof("Available kube cluster: %s", cluster.Name)
+			//kubeCluster = cluster.Name
+		}
+	}
+
+	// ----------------------------------
+	// Get a kubernetes cluster kubeconfig
+	// ----------------------------------
+
+	// Generate a new key pair
+	key, err := tc.KeyFromIdentityFile(opts.TeleportIdentityFile)
+	if key.ClusterName == "" {
+		key.ClusterName = opts.TeleportServer
+	}
+
+	filesWritten, err := identityfile.Write(identityfile.WriteConfig{
+		OutputPath:           "kubeconfig",
+		Key:                  key,
+		Format:               identityfile.FormatKubernetes,
+		KubeProxyAddr:        "https://" + opts.TeleportServer + ":" + opts.TeleportKubeProxyPort,
+		OverwriteDestination: true,
+	})
+	logrus.Infof("kubeconfig written to %v", filesWritten)
+
+	// ----------------------------------
+	// Use the kubeconfig
+	// ----------------------------------
+
+	// Kubernetes
+	config, err := clientcmd.BuildConfigFromFlags("", "./kubeconfig")
+	if err != nil {
+		logrus.Fatalf("failed to config Kubernetes connection: %v", err)
+	}
+
+	clientset := kubernetes.NewForConfigOrDie(config)
+	v, err := clientset.Discovery().ServerVersion()
+	if err != nil {
+		logrus.Fatalf("Unable to determine Kubernetes version: %v", err)
+	}
+
+	logrus.Infof("Kube Version: %s", v.String())
+}
